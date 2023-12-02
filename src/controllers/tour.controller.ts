8ae464e01a5d25a61b4e7671521bc15a602b5212
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-03 00:52:43 +0600

    Refactor Tour Controller

--- src/controllers/tour.controller.ts
+++ src/controllers/tour.controller.ts
@@ -1,40 +1,29 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { tourServices } from "../services/tour.service";
 import { sendSuccessResponse } from "../utils/sendSuccessResponse";
+import { catchAsyncFunction } from "../utils/catchAsyncFunction";
 
-const createTour = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const tourData = req.body;
-    const result = await tourServices.createTour(tourData);
-    sendSuccessResponse(res, {
-      message: "Tour created successfully!",
-      statusCode: 201,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const createTour = catchAsyncFunction(async (req: Request, res: Response) => {
+  const tourData = req.body;
+  const result = await tourServices.createTour(tourData);
+  sendSuccessResponse(res, {
+    message: "Tour created successfully!",
+    statusCode: 201,
+    data: result,
+  });
+});
 
-const getAllTours = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const result = await tourServices.getAllTours();
-    sendSuccessResponse(res, {
-      message: "Tours retrieved successfully!",
-      statusCode: 200,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const getAllTours = catchAsyncFunction(async (req: Request, res: Response) => {
+  const result = await tourServices.getAllTours();
+  sendSuccessResponse(res, {
+    message: "Tours retrieved successfully!",
+    statusCode: 200,
+    data: result,
+  });
+});
 
-const getSingleTour = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
+const getSingleTour = catchAsyncFunction(
+  async (req: Request, res: Response) => {
     const id = req.params.id;
     const result = await tourServices.getSingleTour(id);
     sendSuccessResponse(res, {
@@ -42,46 +31,32 @@ const getSingleTour = async (
       statusCode: 201,
       data: result,
     });
-  } catch (error) {
-    next(error);
-  }
-};
+  },
+);
 
-const updateTour = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const id = req.params.id;
-    const tourData = req.body;
-    const result = await tourServices.updateTour(id, tourData);
-    sendSuccessResponse(res, {
-      message: "Tour updated successfully!",
-      statusCode: 201,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const updateTour = catchAsyncFunction(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const tourData = req.body;
+  const result = await tourServices.updateTour(id, tourData);
+  sendSuccessResponse(res, {
+    message: "Tour updated successfully!",
+    statusCode: 201,
+    data: result,
+  });
+});
 
-const deleteTour = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const id = req.params.id;
-    await tourServices.deleteTour(id);
-    sendSuccessResponse(res, {
-      message: "Tour deleted successfully!",
-      statusCode: 201,
-      data: null,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const deleteTour = catchAsyncFunction(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  await tourServices.deleteTour(id);
+  sendSuccessResponse(res, {
+    message: "Tour deleted successfully!",
+    statusCode: 201,
+    data: null,
+  });
+});
 
-const getNextSchedule = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
+const getNextSchedule = catchAsyncFunction(
+  async (req: Request, res: Response) => {
     const id = req.params.id;
     const tour = await tourServices.getNextSchedule(id);
     sendSuccessResponse(res, {
@@ -89,10 +64,8 @@ const getNextSchedule = async (
       statusCode: 201,
       data: tour,
     });
-  } catch (error) {
-    next(error);
-  }
-};
+  },
+);
 
 export const TourController = {
   createTour,
