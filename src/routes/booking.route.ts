fa127ad857efbb55298f6e75ddca6a2f67878adf
Naimur Reza <naimur.rezaa@gmail.com>
2023-12-03 10:26:34 +0600

    Add bookings route

--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import { BookingController } from "../controllers/booking.controller";
 
 const router = express.Router();
+
+router.get("/", BookingController.getAllBooking);
+
+router.post("/create-booking", BookingController.createBooking);
+
+router.get("/:id", BookingController.getSingleBooking);
+
+router.patch("/:id", BookingController.updateBooking);
+
+router.delete("/:id", BookingController.deleteBooking);
 
 export const bookingRouter = router;
